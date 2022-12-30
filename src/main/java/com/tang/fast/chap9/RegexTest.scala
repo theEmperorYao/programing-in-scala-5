8c114ecda6ac97f469b806ea90ba1b01de47d9c0
tangyao <tangyao@shuzijz.cn>
2022-12-30 19:40:18 +0800

    每日提交

---
+++ src/main/java/com/tang/fast/chap9/RegexTest.scala
@@ -0,0 +1,17 @@
+package com.tang.fast.chap9
+
+import scala.collection.MapOps
+import scala.io.Source
+
+class RegexTest
+
+
+object RegexTest:
+  def main(args: Array[String]): Unit = {
+    val source = Source.fromFile("myfile.txt", "UTF-8")
+    val lineIterator = source.getLines()
+
+    
+
+
+  }
